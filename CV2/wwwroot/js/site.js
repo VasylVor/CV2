885a7bfbcc3a7c7d4a87149daf7b772a8e3a1e9d
Vasyl Voronin <38475725+VasylVor@users.noreply.github.com>
2020-04-23 10:47:57 +0300

    cv
    cv

---
+++ CV2/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+$(document).ready(function () {
+    $('#front').hide();
+    $('#database').hide();   
+});
+
+$(document).ready(function () {
+    $('#card-net').on('click', function () {
+        $('#net').toggle();
+        $('#front').hide();
+        $('#database').hide();
+    });
+});
+
+$(document).ready(function () {
+    $('#card-front').on('click', function () {
+        $('#front').toggle();
+        $('#net').hide();
+        $('#database').hide();
+    });
+});
+
+$(document).ready(function () {
+    $('#card-db').on('click', function () {
+        $('#database').toggle();
+        $('#front').hide();
+        $('#net').hide();
+    });
+});
